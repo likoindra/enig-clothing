052172d97ebe8e28d9e05ac11e1d36e0dd1f13da
likoindra <65221005+likoindra@users.noreply.github.com>
2021-10-17 23:08:57 +0700

    added file for homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import HomePage from "./Homepage/homepage.component";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
