f852f6f7f8e57d083dd2fff1a0b8a1cf13233797
likoindra <65221005+likoindra@users.noreply.github.com>
2021-11-03 10:11:56 +0700

    update app with reselect repplaced cart selectors with memoize selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { toggleCartHidden } from '../../redux/cart/cart.action'
+// method di dalam import ini akan menggantikan method itemCount, yang dimana cartItems.reduce akan dipindah ke cart-selector dan mengembalikan ke dalam cart-icon
+import { selectCartItemsCount } from '../../redux/cart/cart-selector'
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count"> 0 </span>
+    <span className="item-count"> {itemCount}</span>
   </div>
 )
 
@@ -18,5 +20,14 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 })
 
-// passing null as a default 
-export default connect(null, mapDispatchToProps)(CartIcon)
+// method ini akan meng-akumulasi angka yang ada pada dalam quantity
+const mapSateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
+  // cartItems.reduce(
+  //   (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+  //   0,
+  // ),
+})
+
+// passing null as a default
+export default connect(mapSateToProps, mapDispatchToProps)(CartIcon)
